09b1b5e77e6da363817d66e45b3b9d643222c0de
Jen <jen.oen889@gmail.com>
2023-04-07 15:51:08 -0700

    authentication and authorization

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -52,6 +52,7 @@ export const login = async (req, res) => {
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
+    res.status(200).json({ token, user });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
