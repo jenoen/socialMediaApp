478ebb9a8cbd7e91a76941f81c83296138ad6ce8
Jen <jen.oen889@gmail.com>
2023-04-07 15:36:41 -0700

    user and pw credentials

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -37,3 +37,22 @@ export const register = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+// LOGGING IN
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // checks if user email exists
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist." });
+
+    // checks if password matches
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
