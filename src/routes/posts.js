22195da04a6adc44873f7e76b3120552a2fdc9e7
Jen <jen.oen889@gmail.com>
2023-04-10 15:56:15 -0700

    user route setup

---
+++ src/routes/posts.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "./controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// READ
